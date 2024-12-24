363923ba4f89b98756f5e02416ea0e3f3097273c
maximusbaglay <maximusbaglay@gmail.com>
2024-12-25 00:33:09 +0300

    изменено:      __pycache__/server.cpython-311.pyc 	новый файл:    routers/__pycache__/fastapi_task.cpython-311.pyc 	изменено:      routers/fastapi_task.py 	изменено:      server.py

--- routers/fastapi_task.py
+++ routers/fastapi_task.py
@@ -1,10 +1,10 @@
-# from fastapi import APIRouter  
+from fastapi import APIRouter  
   
-# from rabbit.publisher import publish_task_ticker  
+from rabbit.publisher import publish_task_ticker  
   
-# fastapi_task_router = APIRouter(tags=["fastapi_task"])  
+fastapi_task_router = APIRouter(tags=["fastapi_task"])  
   
-# @fastapi_task_router.get("/{ticker}")  
-# async def retrieve_all_records(ticker: str) -> dict:  
-#     await publish_task_ticker(ticker=ticker)  
-#     return {"message": "the ticker has been successfully added to the queue"}
+@fastapi_task_router.get("/{ticker}")  
+async def retrieve_all_records(ticker: str) -> dict:  
+    await publish_task_ticker(ticker=ticker)  
+    return {"message": "the ticker has been successfully added to the queue"}
--- server.py
+++ server.py
@@ -1,13 +1,13 @@
-import uvicorn  
-from fastapi import FastAPI  
-  
-  
-from routers.moex import moex_router  
-  
-app = FastAPI()  
-  
-app.include_router(moex_router, prefix="/moex")  
-  
-  
-if __name__ == "__main__":  
+import uvicorn
+from fastapi import FastAPI
+
+
+from routers.fastapi_task import fastapi_task_router
+
+app = FastAPI()
+
+app.include_router(fastapi_task_router, prefix="/main")
+
+
+if __name__ == "__main__":
     uvicorn.run("server:app", host="127.0.0.1", port=5000, reload=True)
